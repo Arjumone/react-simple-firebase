6b170436c40239337961b7c1d4de4a693b3a813a
Arju Mone <121635205+Arjumone@users.noreply.github.com>
2023-10-06 12:35:39 +0600

    github by firebase

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,45 +1,71 @@
-import { GoogleAuthProvider, getAuth, signInWithPopup, signOut } from "firebase/auth";
+import {
+  GithubAuthProvider,
+  GoogleAuthProvider,
+  getAuth,
+  signInWithPopup,
+  signOut,
+} from "firebase/auth";
 import app from "../../firebase/firebase.config";
 import { useState } from "react";
 
 const Login = () => {
-    const [user,setUser]=useState(null)
-    const auth = getAuth(app);
-    const googleProvider = new GoogleAuthProvider();
+  const [user, setUser] = useState(null);
+  const auth = getAuth(app);
+  const googleProvider = new GoogleAuthProvider();
+  const githubProvider = new GithubAuthProvider();
 
-    const handleGoogleSignIn = ()=>{
-        signInWithPopup(auth,googleProvider)
+  const handleGoogleSignIn = () => {
+    signInWithPopup(auth, googleProvider)
+      .then((result) => {
+        const loggedUser = result.user;
+        setUser(loggedUser);
+      })
+      .catch((error) => {
+        console.log(error.message);
+      });
+  };
+  const handleGithubLogin=()=>{
+        signInWithPopup(auth,githubProvider)
         .then(result=>{
-        const loggedUser=result.user;
+            const loggedUser = result.user
+            console.log(loggedUser);
             setUser(loggedUser)
-        })
-        .catch(error=>{
-            console.log(error.message);
-        })
-    }
-    const handleSignOut =()=>{
-        signOut(auth)
-        .then(result=>{
-            console.log(result);
-            setUser(null)
         })
         .catch(error=>{
             console.log(error);
         })
-    }
-    return (
+  }
+
+  const handleSignOut = () => {
+    signOut(auth)
+      .then((result) => {
+        console.log(result);
+        setUser(null);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+  return (
+    <div>
+      {/* user? logout : sign in */}
+      {user ? (
+        <button onClick={handleSignOut}>Sign Out</button>
+      ) : (
         <div>
-            {/* user? logout : sign in */}
-            {user ? <button onClick={handleSignOut}>Sign Out</button> 
-            :
-            <button onClick={handleGoogleSignIn}>Google login</button>}
-            {user && <div>
-                <h2>User:{user.displayName}</h2>
-                <p>{user.email}</p>
-                <img src={user.photoURL} alt="" />
-            </div>}
+          <button onClick={handleGoogleSignIn}>Google login</button>
+          <button onClick={handleGithubLogin}>Github login</button>
         </div>
-    );
+      )}
+      {user && (
+        <div>
+          <h2>User:{user.displayName}</h2>
+          <p>{user.email}</p>
+          <img src={user.photoURL} alt="" />
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default Login;
